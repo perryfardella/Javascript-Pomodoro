98e63878814679c8070c2e438052ea2047ed73e0
DESKTOP-AO9R47R\Perry <perry.fardella@live.com>
2020-10-13 20:41:52 +0800

    Initial draft of getTimeFromUser method, need to error handle better, also the recursive call of the functiond oesn't work because the readline interface doesn't allow for itself to be called mroe than once.

--- pomodoro.js
+++ pomodoro.js
@@ -4,6 +4,25 @@
     Author: Perry Fardella
 */
 
+// Initialise a readline interface
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+//Initialise a variable to store the pomodoro time
+var time;
+
 function getTimeFromUser() {
-    
+    readline.question('How long would you like to set the pomodoro for? (in minutes)\n', inputTime => {
+        if (isNaN(inputTime)) {
+            console.log("You did not enter a number, try again.");
+            getTimeFromUser();
+        } else {
+            time = inputTime;
+        }
+        readline.close();
+      });
 }
+
+getTimeFromUser();
